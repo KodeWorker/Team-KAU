4e5d960e9fb277f8ecda2b6d2ceb5da6f1093cd0
KodeWorker <fxp61005@gmail.com>
2020-08-17 14:13:17 +0800

    20200817 update

--- data_preparation/unzip_all_folds.py
+++ data_preparation/unzip_all_folds.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     data_dir = args.source
     extract_dir = args.target
     
-    filepaths = glob.glob(data_dir + "\*.zip")
+    filepaths = glob.glob(data_dir + "/*.zip")
     
     if not os.path.exists(extract_dir):
         os.makedirs(extract_dir)
